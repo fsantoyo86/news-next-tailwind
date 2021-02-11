42f14193cb18b24b558de614caacfd2366d59955
FSantoyo125 <pacolin86@gmail.com>
2021-02-11 15:55:25 -0600

    profile commit

--- pages/eom.js
+++ pages/eom.js
@@ -2,19 +2,22 @@ import { Toolbar } from '../components/toolbar';
 import styles from '../styles/EOM.module.css';
 
 export const EOM = ({employee}) => {
-return(
-    <div className="page-container">
-        <Toolbar></Toolbar>
-        <div className={styles.main}>
-            <h1>About us</h1>
-            <div className={styles.employeeOfTheMonth}>
-                <h3>{employee.name}</h3>
-                <h6>{employee.position}</h6>
-                <img src={employee.image} alt="profile"/>
-                <p>{employee.description}</p>
-            </div>
-        </div>
+return (
+  <div className="page-container">
+    <Toolbar></Toolbar>
+    <div className={styles.main}>
+      <h1>About us</h1>
+      <div className={styles.employeeOfTheMonth}>
+        <h3>{employee.name}</h3>
+        <h6>Reporter</h6>
+        <img
+          src="https://store.playstation.com/store/api/chihiro/00_09_000/container/MX/es/999/UP2538-CUSA05620_00-AV00000000000209/1601167370000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000"
+          alt="profile"
+        />
+        <p>{employee.description}</p>
+      </div>
     </div>
+  </div>
 );
 };
 
