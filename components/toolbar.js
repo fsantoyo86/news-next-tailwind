fbd06d26f1c5ad090fa743b49bf6a4aa4d078c21
FSantoyo125 <pacolin86@gmail.com>
2021-02-10 20:39:41 -0600

    first commit

---
+++ components/toolbar.js
@@ -0,0 +1,24 @@
+import {useRouter} from 'next/router';
+import styles from '../styles/Toolbar.module.css';
+
+
+export const Toolbar =()=>{
+    const router = useRouter();
+
+    return (
+      <div className={styles.main}>
+        <div className="" onClick={() => router.push("/")}>
+          Home
+        </div>
+        <div className="" onClick={() => router.push("/feed/1")}>
+          Feed
+        </div>
+        <div className="" onClick={() => router.push("/eom")}>
+          EOM
+        </div>
+        <div className="" onClick={() => window.location.href='https://twitter.com'}>
+          Twitter
+        </div>
+      </div>
+    );
+};
